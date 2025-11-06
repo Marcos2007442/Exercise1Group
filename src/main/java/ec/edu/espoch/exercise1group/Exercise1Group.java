d7f1b99b65f9f6c358336ec7c2419a1704e10a55
Marcos2007442 <mg260307@gmail.com>
2025-11-06 14:32:46 -0500

    creacion de las clases

---
+++ src/main/java/ec/edu/espoch/exercise1group/Exercise1Group.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.exercise1group;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Exercise1Group {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
